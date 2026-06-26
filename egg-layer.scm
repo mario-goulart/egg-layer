mario-goulart/egg-layer

commit 0be8f0bc0dec887bdd919969e492ecacfc80de36

When changing directories with cd, prefix paths with ./
Just in case CDPATH is set to something that conflicts with the
argument of cd.

egg-layer.scm
@@ -287,7 +287,7 @@
                                                    "egg-info")))
                                (string-append
                                 "( "
-                                (sprintf "cd ~a && $(CHICKEN_INSTALL) && " src-dir)
+                                (sprintf "cd ./~a && $(CHICKEN_INSTALL) && " src-dir)
                                 ;; This is absolutely horrendous, but from
                                 ;; CHICKEN 5.4.0 on the VERSION file is read
                                 ;; from $CHICKEN_EGG_CACHE/.cache-metadata, and

commit 03a293181b15b2e910571a32779fc4df9669065b

Fix installation of local eggs
0be8f0bc0de introduced a regression for the installation of local
eggs, as the path to the egg sources directory is absolute, so it
cannot be prefixed with `./`.

egg-layer.scm
@@ -287,7 +287,10 @@
                                                    "egg-info")))
                                (string-append
                                 "( "
-                                (sprintf "cd ./~a && $(CHICKEN_INSTALL) && " src-dir)
+                                (sprintf "cd ~a && $(CHICKEN_INSTALL) && "
+                                         (if local-egg
+                                             src-dir
+                                             (make-pathname "." src-dir)))
                                 ;; This is absolutely horrendous, but from
                                 ;; CHICKEN 5.4.0 on the VERSION file is read
                                 ;; from $CHICKEN_EGG_CACHE/.cache-metadata, and